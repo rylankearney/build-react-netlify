3a5cc84a2e0b96ff1ac3be9eb47c2b814d31f5e6
rylankearney <rylan.isabel@live.ca>
2022-11-28 12:18:10 -0500

    test #2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is the PRODUCTION branch</h1>
+      <h1>Hello. This is the PRODUCTION branch</h1>
     </div>
   );
 }
